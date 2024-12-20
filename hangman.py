0e3b4774cdcfba1a13469fa87185a1f30121f271
firattaha <tahafiratt23@gmail.com>
2024-12-20 13:51:15 +0300

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -9,3 +9,14 @@ gorunur_kelime = ["_" for _ in secilen_kelime]
 yanlis_tahminler = 0
 maksimum_hak = 7
 toplam_puan = 0  # Puanlama için başlangıç değeri
+
+
+def kelime_sec(dosya_adi):
+    try:
+        with open(dosya_adi, "r", encoding="utf-8") as dosya:
+            kelimeler = dosya.read().splitlines()
+        return random.choice(kelimeler).lower()
+    except FileNotFoundError:
+        messagebox.showerror("Hata", "Kelimeler dosyası bulunamadı!")
+        exit()
+
