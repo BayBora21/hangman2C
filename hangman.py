e0e2f83260b76c8bf3283b39c7f636651e1d884c
firattaha <tahafiratt23@gmail.com>
2024-12-20 13:50:28 +0300

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -3,4 +3,9 @@ import random
 
 import os
 import sys
-
+dosya_adi = "kelimeler.txt"  # Kelimelerin olduğu dosya adı
+secilen_kelime = kelime_sec(dosya_adi)
+gorunur_kelime = ["_" for _ in secilen_kelime]
+yanlis_tahminler = 0
+maksimum_hak = 7
+toplam_puan = 0  # Puanlama için başlangıç değeri
